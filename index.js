7c739faaac53a71c425b843ea8ae31ec2de74dc1
tharunkumar <tharunkumar9159@gmail.com>
2024-03-19 21:30:05 +0530

    code added

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// document.write("tharunkumar")
+// var state = {
+//   taskList: [
+//     {
+//       imageUrl: "",
+//       taskTitle: "",
+//       taskType: "",
+//       taskDescription: "",
+//     },
+//     {
+//       imageUrl: "",
+//       taskTitle: "",
+//       taskType: "",
+//       taskDescription: "",
+//     },
+//     {
+//       imageUrl: "",
+//       taskTitle: "",
+//       taskType: "",
+//       taskDescription: "",
+//     },
+//     {
+//       imageUrl: "",
+//       taskTitle: "",
+//       taskType: "",
+//       taskDescription: "",
+//     },
+//     {
+//       imageUrl: "",
+//       taskTitle: "",
+//       taskType: "",
+//       taskDescription: "",
+//     },
+//   ],
+// };
+
+const state = {
+  taskList: [],
+};
+
+// DOM operations
+const taskContents = document.querySelector(".task__contents");
+const taskModal = document.querySelector(".task__modal__body");
+
+// console.log(taskContents);
+// console.log(taskModal);
+
+const htmlTaskContents = ({ id, title, description, type, url }) => {};
